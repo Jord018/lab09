deb6a9e4fb1326671c15fb36c1ff586495dd1f8f
Jord <jord018@users.noreply.github.com>
2026-09-13 18:16:11 +0700

    2.2

--- src/main/java/com/example/demo/entity/Event.java
+++ src/main/java/com/example/demo/entity/Event.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,6 +31,8 @@ public class Event {
     String date;
     String time;
     Boolean petsAllowed;
+    @ManyToOne
     Organizer organizer;
+    @ManyToMany(mappedBy = "eventHistories")
     List<Participant> participants;
 }
